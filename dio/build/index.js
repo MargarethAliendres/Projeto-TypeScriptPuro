d4b7a15a375f3a43bcc325392fcc8106ea6efece
margareth <margareth.aliendres@gmail.com>
2023-04-21 18:16:14 -0300

    first commit

---
+++ dio/build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+function printars(d) {
+    console.log(d);
+}
+printars({
+    der: "mar",
+    ger: "",
+});
+let aniver = new Date("2023-04-20 05:00");
+console.log(aniver.toString());
+function soma(x, y) {
+    return x + y;
+}
+let som = soma(7, 8);
+console.log(som);
---
+++ dio/src/index.ts
@@ -0,0 +1,26 @@
+type mar = {
+    der: string;
+    ger: string;
+};
+
+function printars(d: mar){
+console.log(d);
+
+
+}
+printars({
+    der: "mar",
+    ger: "",
+});
+
+let aniver: Date = new Date("2023-04-20 05:00");
+console.log(aniver.toString());
+
+function soma(x:number, y:number){
+    return x+y;
+
+}
+
+let som: number = soma(7, 8);
+
+console.log(som);
